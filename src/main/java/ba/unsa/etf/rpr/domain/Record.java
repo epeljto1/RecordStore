d143b2b179d8d3ca64ae5b8772229f5166742276
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-09 16:30:26 +0200

    Finish record model implementation

--- src/main/java/ba/unsa/etf/rpr/domain/Record.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Record.java
@@ -23,4 +23,52 @@ public class Record {
         this.genre = genre;
         this.country = country;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
