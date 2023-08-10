2ffda77f86f323eb3cc8f96af539c1cd3ef534d7
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 12:35:50 +0200

    Updated LabelDao

--- src/main/java/ba/unsa/etf/rpr/dao/LabelDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/LabelDao.java
@@ -2,9 +2,14 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Label;
 
+import java.util.List;
+
 /**
  * DAO interface for Label model
  */
 
 public interface LabelDao extends Dao<Label> {
+    // Returns list of all labels with the same name
+    // as search parameter
+    List<Label> searchByName(String name);
 }
