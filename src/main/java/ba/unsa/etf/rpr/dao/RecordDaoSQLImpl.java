f2cda6e89a73d7d3c1a5be68dc951e10045fb4fa
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 16:33:09 +0200

    Updated RecordDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/RecordDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordDaoSQLImpl.java
@@ -1,11 +1,14 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Record;
+
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.List;
 import java.util.Properties;
 
-public class RecordDaoSQLImpl {
+public class RecordDaoSQLImpl implements RecordDao {
     private Connection conn;
 
     public RecordDaoSQLImpl() {
@@ -18,5 +21,34 @@ public class RecordDaoSQLImpl {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public Record getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Record add(Record item)
+    {
+        return null;
+    }
+
+    @Override
+    public Record update(Record item)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(int id)
+    {
+
+    }
+
+    @Override
+    public List<Record> getAll()
+    {
+        return null;
     }
 }
