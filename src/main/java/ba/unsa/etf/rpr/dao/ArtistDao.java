ccbaa8bc730129a9d51d2713344ba9b07c093f0a
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 12:33:22 +0200

    Updated ArtistDao

--- src/main/java/ba/unsa/etf/rpr/dao/ArtistDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtistDao.java
@@ -2,9 +2,14 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Artist;
 
+import java.util.List;
+
 /**
  * DAO interface for Artist model
  */
 
 public interface ArtistDao extends Dao<Artist> {
+    // Returns list of all artists with the same name
+    // as search parameter
+    List<Artist> searchByName(String name);
 }
