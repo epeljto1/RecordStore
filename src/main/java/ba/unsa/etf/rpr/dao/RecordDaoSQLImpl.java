9133ec792fa25068f46f71159478d09fb3d7f775
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 16:29:53 +0200

    Added RecordDaoSQLimpl connection

--- src/main/java/ba/unsa/etf/rpr/dao/RecordDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordDaoSQLImpl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 public class RecordDaoSQLImpl {
+    private Connection conn;
+
+    public RecordDaoSQLImpl() {
+        Properties prop = new Properties();
+        try(InputStream input = LabelDaoSQLImpl.class.getResourceAsStream("/application.properties"))
+        {
+            prop.load(input);
+            conn = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("username"), prop.getProperty("password"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
