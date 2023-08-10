409e858db0c008d7bce5623bb1c24d8954f09404
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 12:48:35 +0200

    ArtistDaoSQLImpl connection

--- src/main/java/ba/unsa/etf/rpr/dao/ArtistDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ArtistDaoSQLImpl.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.dao;
 
-public class ArtistDaoSQLImpl {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ArtistDaoSQLImpl implements ArtistDao {
+    private Connection conn;
+
+    public ArtistDaoSQLImpl() {
+        Properties prop = new Properties();
+        try
+        {
+            this.conn = DriverManager.getConnection("jdbc:mysql://" +
+                            prop.getProperty("db.host") + "/" +
+                            prop.getProperty("db.scheme") + "/" +
+                            prop.getProperty("db.port"),
+                    prop.getProperty("db.username"), prop.getProperty("db.password"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
