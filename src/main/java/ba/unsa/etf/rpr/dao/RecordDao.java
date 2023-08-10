113a17d608d50da4c407a02ef4196ac09c489f41
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-10 12:37:36 +0200

    Updated RecordDao

--- src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
@@ -2,9 +2,14 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Record;
 
+import java.util.List;
+
 /**
  * DAO interface for Record model
  */
 
 public interface RecordDao extends Dao<Record> {
+    // Returns list of all records with the same name
+    // as search parameter
+    List<Record> searchbyName(String name);
 }
