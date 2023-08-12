3bf82848a43d8ec8f40968cf9da87b0257ebea09
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-12 16:34:32 +0200

    Cleared main class

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -16,42 +16,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Test getById - LabelDao
-        // LabelDao labelDao = new LabelDaoSQLImpl();
-        // int id = 1;
-        // Label label = labelDao.getById(id);
-        // if (label.getName().equals("Mute Records")) {
-           // System.out.println("The strings are equal.");
-        // } else {
-           // System.out.println("The strings are not equal.");
-        // }
-
-        // Test add - LabelDao
-        // LabelDao labelDao = new LabelDaoSQLImpl();
-        // Label label = new Label();
-        // label.setName("Parkwood Entertainment");
-        // label.setCountry("United States");
-        // labelDao.add(label);
-
-
-
-         // ArtistDao artistDao = new ArtistDaoSQLImpl();
-         // Artist artist = artistDao.searchByLabel("Parkwood Entertainment").get(0);
-        // if (artist.getName().equals("Beyoncé") && artist.getLabel().getCountry().equals("United States")) {
-         // System.out.println("The strings are equal.");
-          // } else {
-         // System.out.println("The strings are not equal.");
-          // }
-
-        // LabelDao labelDao = new LabelDaoSQLImpl();
-        // if (labelDao.searchByCountry("United States").size()==2 && labelDao.searchByCountry("United States").get(1).getName().equals("Columbia Records")) {
-        // System.out.println("Okay.");
-        // } else {
-         // System.out.println("Not okay.");
-         // }
-
-        RecordDao recordDao = new RecordDaoSQLImpl();
-        recordDao.delete(2);
 
     }
 }
