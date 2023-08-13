96988ba04525ddb403c236bb14e0f60125ebd1e9
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-13 12:32:26 +0200

    Fixed abstract DAO class

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.dao;
 
-public class AbstractDao {
+import java.sql.Connection;
+
+/**
+ * Abstract class for DAO CRUD methods
+ */
+
+public abstract class AbstractDao<T> implements Dao<T> {
+    private Connection conn;
+    private String table;
 }
