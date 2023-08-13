49fe8d84615367639bbe597af07560d4d66663c0
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-13 12:59:58 +0200

    Added throws RecordStoreException to RecordDao class

--- src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecordDao.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.exceptions.RecordStoreException;
 
 import java.util.Date;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
 public interface RecordDao extends Dao<Record> {
     // Returns list of all records with the same name
     // as search parameter
-    List<Record> searchByName(String name);
-    List<Record> searchByArtist(String name);
-    List<Record> searchByDateRange(Date start, Date end);
+    List<Record> searchByName(String name) throws RecordStoreException;
+    List<Record> searchByArtist(String name) throws RecordStoreException;
+    List<Record> searchByDateRange(Date start, Date end) throws RecordStoreException;
 }
