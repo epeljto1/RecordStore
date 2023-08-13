2da739521326b7ea1a7c7801bf3a90c531f01fc7
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-13 12:35:42 +0200

    Added constructor to AbstractDao class

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 /**
  * Abstract class for DAO CRUD methods
@@ -9,4 +11,16 @@ import java.sql.Connection;
 public abstract class AbstractDao<T> implements Dao<T> {
     private Connection conn;
     private String table;
+
+    public AbstractDao(String tableName) {
+        try {
+            this.table = table;
+            Properties p = new Properties();
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            this.conn = DriverManager.getConnection(p.getProperty("url"), p.getProperty("username"), p.getProperty("password"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
