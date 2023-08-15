d50772133b4b5f524e8599355253ac3143f2930f
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-15 15:24:16 +0200

    Finished DaoFactory class

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,24 @@
 package ba.unsa.etf.rpr.dao;
 
 public class DaoFactory {
+    private static final LabelDao labelDao = new LabelDaoSQLImpl();
+    private static final ArtistDao artistDao = new ArtistDaoSQLImpl();
+    private static final RecordDao recordDao = new RecordDaoSQLImpl();
+
+    private DaoFactory(){}
+
+    public static LabelDao labelDao()
+    {
+        return labelDao;
+    }
+
+    public static ArtistDao artistDao()
+    {
+        return artistDao;
+    }
+
+    public static RecordDao recordDao()
+    {
+        return recordDao;
+    }
 }
