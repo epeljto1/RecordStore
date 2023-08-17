b6a0e39ae19493220780bb9f5825012a098fe8a9
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 22:21:21 +0200

    Removed home controller and home layout

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,13 +1,7 @@
 package ba.unsa.etf.rpr;
 
-import ba.unsa.etf.rpr.controllers.HomeController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 /**
  * Main class for working with JavaFX framework
@@ -17,13 +11,9 @@ public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws Exception
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home-layout.fxml"));
-        HomeController homeController = new HomeController();
-        fxmlLoader.setController(homeController);
-        Parent root = fxmlLoader.load();
         stage.setTitle("RecordStore");
-        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.setResizable(false);
         stage.show();
     }
+
 }
