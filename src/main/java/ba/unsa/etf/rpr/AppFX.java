38d530eca170bddac005e08227e6b3844ac151c1
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 15:16:10 +0200

    Updated AppFX class

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,8 +1,13 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.controllers.HomeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 /**
  * Main class for working with JavaFX framework
@@ -13,5 +18,12 @@ public class AppFX extends Application {
     public void start(Stage stage) throws Exception
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home-layout.fxml"));
+        HomeController homeController = new HomeController();
+        fxmlLoader.setController(homeController);
+        Parent root = fxmlLoader.load();
+        stage.setTitle("RecordStore");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
     }
 }
