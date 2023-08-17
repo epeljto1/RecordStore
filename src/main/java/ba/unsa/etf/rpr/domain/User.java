210ac1dddbed1ebf08b10e271c499725753b02c5
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 22:30:04 +0200

    Updated User class

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.domain;
 
 public class User implements Identifiable {
+    private int id;
+    private String username;
+    private String password;
+
+    @Override
+    public int getId() { return id; }
+
+    @Override
+    public void setId(int id) { this.id = id; }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
