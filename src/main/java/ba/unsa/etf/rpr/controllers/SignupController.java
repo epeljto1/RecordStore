f71db82810233134b35bb3b6f2d3c8ca762e4082
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 23:45:14 +0200

    Added sign up controller

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public PasswordField confirmPasswordField;
+}
