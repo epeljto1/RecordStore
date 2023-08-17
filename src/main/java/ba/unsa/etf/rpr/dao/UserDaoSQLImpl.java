fbdd826db0bda2f8f147d4fedced50505e9b0895
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 22:39:47 +0200

    Added row2Object method to UserDaoSQLImpl class

--- src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/UserDaoSQLImpl.java
@@ -1,11 +1,31 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Record;
 import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.RecordStoreException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDaoSQLImpl extends AbstractDao<User> implements UserDao {
     private Connection conn;
 
     public UserDaoSQLImpl() { super("users"); }
+
+    @Override
+    public User row2Object(ResultSet rs) throws RecordStoreException
+    {
+        try {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+        catch (SQLException e)
+        {
+            throw new RecordStoreException(e.getMessage(),e);
+        }
+    }
 }
