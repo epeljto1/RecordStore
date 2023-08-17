41645756f25a16b658a3c408b79080206a6c07fd
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-17 14:46:00 +0200

    Added home layout

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,10 +1,17 @@
 package ba.unsa.etf.rpr;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 /**
  * Main class for working with JavaFX framework
  */
 
 public class AppFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home-layout.fxml"));
+    }
 }
