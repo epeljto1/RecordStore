48330a8c958b6b7032c08c68e87e43fe5ec58def
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-18 22:43:23 +0200

    Added getAll to UserManager class

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -4,6 +4,8 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.RecordStoreException;
 
+import java.util.List;
+
 /**
  * Business logic layer for User
  * @author Emina Peljto
@@ -19,11 +21,6 @@ public class UserManager {
     public User getById(int id) throws RecordStoreException
     {
         return DaoFactory.userDao().getById(id);
-    }
-
-    public User getUser(String username, String password) throws RecordStoreException
-    {
-        return DaoFactory.userDao().getUser(username,password);
     }
 
     /**
@@ -56,5 +53,27 @@ public class UserManager {
     public void delete(int id) throws RecordStoreException
     {
         DaoFactory.userDao().delete(id);
+    }
+
+    /**
+     * Returns all users from DB
+     * @return list of users
+     * @throws RecordStoreException
+     */
+    public List<User> getAll() throws RecordStoreException
+    {
+        return DaoFactory.userDao().getAll();
+    }
+
+    /**
+     * Returns user from DB
+     * @param username
+     * @param password
+     * @return user
+     * @throws RecordStoreException
+     */
+    public User getUser(String username, String password) throws RecordStoreException
+    {
+        return DaoFactory.userDao().getUser(username,password);
     }
 }
