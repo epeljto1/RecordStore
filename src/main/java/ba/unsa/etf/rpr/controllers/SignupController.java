cd992223b2a257388e47a396d889293946f71484
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-18 23:09:07 +0200

    Updated sign in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SignupController.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.UserManager;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -8,4 +10,35 @@ public class SignupController {
     public TextField usernameField;
     public PasswordField passwordField;
     public PasswordField confirmPasswordField;
+
+    private final UserManager userManager = new UserManager();
+
+    public void initialize() {
+        Platform.runLater(() -> usernameField.requestFocus());
+
+        usernameField.textProperty().addListener((observable, o, n) -> {
+            if(n.trim().isEmpty()) setInvalidStyles(usernameField);
+            else removeInvalidStyles(usernameField);
+        });
+
+        passwordField.textProperty().addListener((observable, o, n) -> {
+            if(n.trim().isEmpty()) setInvalidStyles(passwordField);
+            else removeInvalidStyles(passwordField);
+        });
+
+        confirmPasswordField.textProperty().addListener((observable, o, n) -> {
+            if(n.trim().isEmpty() || !n.equals(passwordField.getText())) setInvalidStyles(confirmPasswordField);
+            else removeInvalidStyles(confirmPasswordField);
+        });
+    }
+
+    private void setInvalidStyles(TextField textField) {
+        textField.getStyleClass().removeAll("default");
+        textField.getStyleClass().add("invalid");
+    }
+
+    private void removeInvalidStyles(TextField textField) {
+        textField.getStyleClass().removeAll("invalid");
+        textField.getStyleClass().add("default");
+    }
 }
