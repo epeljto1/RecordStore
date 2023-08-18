e7a99e57725a338fc6935aa1ff7d03b745be2465
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-18 22:32:59 +0200

    Added getById to UserManager class

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -4,7 +4,23 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exceptions.RecordStoreException;
 
+/**
+ * Business logic layer for User
+ * @author Emina Peljto
+ */
+
 public class UserManager {
+    /**
+     * Returns a user from DB based on ID
+     * @param id
+     * @return User
+     * @throws RecordStoreException
+     */
+    public User getById(int id) throws RecordStoreException
+    {
+        return DaoFactory.userDao().getById(id);
+    }
+
     public User getUser(String username, String password) throws RecordStoreException
     {
         return DaoFactory.userDao().getUser(username,password);
