d0a0b08ff5464d213963de1e28e018de8536fce7
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-19 14:28:03 +0200

    Updated log in controller

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.business.TabManager;
 import ba.unsa.etf.rpr.business.UserManager;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -13,4 +14,28 @@ public class LoginController {
 
     private final TabManager windowManager = new TabManager();
     private final UserManager userManager = new UserManager();
+
+    public void initialize() {
+        Platform.runLater(() -> usernameField.requestFocus()); // needed to set focus on username field
+
+        usernameField.textProperty().addListener((observable, o, n) -> {
+            if(n.trim().isEmpty()) setInvalidStyles(usernameField);
+            else removeInvalidStyles(usernameField);
+        });
+
+        passwordField.textProperty().addListener((observable, o, n) -> {
+            if(n.trim().isEmpty()) setInvalidStyles(passwordField);
+            else removeInvalidStyles(passwordField);
+        });
+    }
+
+    private void setInvalidStyles(TextField textField) {
+        textField.getStyleClass().removeAll("default");
+        textField.getStyleClass().add("invalid");
+    }
+
+    private void removeInvalidStyles(TextField textField) {
+        textField.getStyleClass().removeAll("invalid");
+        textField.getStyleClass().add("default");
+    }
 }
