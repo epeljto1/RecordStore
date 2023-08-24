a3fd39db6aca1f9a96ec57d0a004951eb2ad90c8
epeljto1 <epeljto1@etf.unsa.ba>
2023-08-24 12:29:02 +0200

    Created manager class for Record and added getById method

---
+++ src/main/java/ba/unsa/etf/rpr/business/RecordManager.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Record;
+import ba.unsa.etf.rpr.exceptions.RecordStoreException;
+
+public class RecordManager {
+    public Record getById(int id) throws RecordStoreException
+    {
+        return DaoFactory.recordDao().getById(id);
+    }
+}
